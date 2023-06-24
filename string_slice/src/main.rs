1c32f4700a1d95c2aacb72dae696e943a8b0addc
o0olele <o0olele@github.com>
2023-06-24 16:02:35 +0800

    [11.string_slice] update code and md

--- string_slice/src/main.rs
+++ string_slice/src/main.rs
@@ -1,10 +1,22 @@
 #![allow(unused_variables)]
+
+use std::ops::Add;
 fn main() {
     print_bad_string();
 
     slice_example();
     slice_borrow();
     slice_integer();
+
+    string_convert();
+    string_index();
+    string_push();
+    string_replace();
+    string_delete();
+    string_concatenate();
+    string_escape();
+    string_utf8();
+    string_drop();
 }
 
 fn greet(name: String) {
@@ -56,4 +68,161 @@ fn slice_integer() {
     let slice = &a[1..3];
 
     assert_eq!(slice, &[2, 3]);
+}
+
+fn say_hello(s: &str) {
+    println!("{}", s);
+}
+
+fn string_convert() {
+    let s1 = String::from("hello");
+    let s2 = "world".to_string();
+
+    say_hello(&s1);
+    say_hello(&s2);
+
+    say_hello(&s1[1..]);
+    say_hello(s1.as_str());
+
+    println!();
+}
+
+fn string_index() {
+    let s1 = String::from("hello");
+
+    // let h = s1[0];
+    // let h = &s1[0];
+}
+
+fn string_push() {
+    let mut s1 = String::from("hello world");
+
+    s1.insert(5, ',');
+    println!("insert -> {}", s1);
+
+    s1.insert_str(6, "you are ");
+    println!("insert -> {}", s1);
+
+    println!();
+}
+
+fn string_replace() {
+    let mut string_replace = String::from("I like rust. Learning rust is my favorite!");
+
+    let new_string_replace = string_replace.replace("rust", "RUST");
+    dbg!(new_string_replace);
+
+    let new_string_replacen = string_replace.replacen("rust", "RUST", 1);
+    dbg!(new_string_replacen);
+
+    string_replace.replace_range(7..8, "R");
+    dbg!(string_replace);
+
+    println!();
+}
+
+fn string_delete() {
+    let mut string_pop = String::from("rust pop 中文!");
+    let p1 = string_pop.pop();
+    let p2 = string_pop.pop();
+    dbg!(p1);
+    dbg!(p2);
+    dbg!(string_pop);
+
+    let mut string_remove = String::from("测试remove方法");
+    println!(
+        "string_remove 占 {} 个字节",
+        std::mem::size_of_val(string_remove.as_str())
+    );
+    // 删除第一个汉字
+    println!("{}", string_remove.remove(0));
+    // 下面代码会发生错误
+    // string_remove.remove(1);
+    // 直接删除第二个汉字
+    // string_remove.remove(3);
+    dbg!(string_remove);
+
+    let mut string_truncate = String::from("测试truncate");
+    string_truncate.truncate(3);
+    dbg!(string_truncate);
+
+    let mut string_clear = String::from("string clear");
+    string_clear.clear();
+    dbg!(string_clear);
+}
+
+fn string_concatenate() {
+    let s1 = String::from("hello");
+    let s2 = String::from("world");
+
+    // let s3 = s1.add(&s2);
+    let s3 = s1 + &s2;
+    let mut s3 = s3 + "!";
+    dbg!(s3);
+
+    let s1 = String::from("hello,");
+    let s2 = String::from("world!");
+
+    let s3 = s1 + &s2;
+    assert_eq!(s3, "hello,world!");
+    // println!("{}",s1);
+
+    let s1 = "hello";
+    let s2 = String::from("rust");
+    let s = format!("{} {}!", s1, s2);
+    println!("{}", s);
+
+    println!();
+}
+
+fn string_escape() {
+    let byte_escape = "I'm writing \x52\x75\x73\x74!";
+    println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!(
+        "Unicode character {} (U+211D) is called {}",
+        unicode_codepoint, character_name
+    );
+
+    let long_string = "String literals
+    can span multiple lines.
+                        The linebreak and indentation here ->\
+                        <- can be escaped too!";
+    println!("{}", long_string);
+
+    println!("{}", "hello \\x52\\x75\\x73\\x74");
+    let raw_str = r"Escapes don't work here: \x3F \u{211D}";
+    println!("{}", raw_str);
+
+    // 如果字符串包含双引号，可以在开头和结尾加 #
+    let quotes = r#"And then I said: "There is no escape!""#;
+    println!("{}", quotes);
+
+    // 如果还是有歧义，可以继续增加，没有限制
+    let longer_delimiter = r###"A string with "# in it. And even "##!"###;
+    println!("{}", longer_delimiter);
+
+    println!();
+}
+
+fn string_utf8() {
+    for c in "中国人".chars() {
+        println!("{}", c);
+    }
+
+    for b in "中国人".bytes() {
+        println!("{}", b);
+    }
+}
+
+fn string_drop() {
+    {
+        let s = String::from("hello"); // 从此处起，s 是有效的
+    
+        // 使用 s
+    }                                  // 此作用域已结束，
+                                       // s 不再有效，内存被释放
 }
--- string_slice/zh.md
+++ string_slice/zh.md
@@ -1,6 +1,74 @@
 # 原文
+- [字符串](https://course.rs/basic/compound-type/string-slice.html#%E8%BF%9E%E6%8E%A5-concatenate)
 
 # 运行结果
+~~~shell
+hello rust
+0x564bb5ad9ba6 0x564bb5ad9ba6
+泪
 
+h
+
+hello
+world
+ello
+hello
+
+insert -> hello, world
+insert -> hello,you are  world
+
+[src/main.rs:113] new_string_replace = "I like RUST. Learning RUST is my favorite!"
+[src/main.rs:116] new_string_replacen = "I like RUST. Learning rust is my favorite!"
+[src/main.rs:119] string_replace = "I like Rust. Learning rust is my favorite!"
+
+[src/main.rs:128] p1 = Some(
+    '!',
+)
+[src/main.rs:129] p2 = Some(
+    '文',
+)
+[src/main.rs:130] string_pop = "rust pop 中"
+string_remove 占 18 个字节
+测
+[src/main.rs:143] string_remove = "试remove方法"
+[src/main.rs:147] string_truncate = "测"
+[src/main.rs:151] string_clear = ""
+[src/main.rs:161] s3 = "helloworld!"
+hello rust!
+
+What are you doing? (\x3F means ?) I'm writing Rust!
+Unicode character ℝ (U+211D) is called "DOUBLE-STRUCK CAPITAL R"
+String literals
+    can span multiple lines.
+                        The linebreak and indentation here -><- can be escaped too!
+hello \x52\x75\x73\x74
+Escapes don't work here: \x3F \u{211D}
+And then I said: "There is no escape!"
+A string with "# in it. And even "##!
+
+中
+国
+人
+228
+184
+173
+229
+155
+189
+228
+186
+186
+~~~
 # 一些问题
 - 在`golang`里面，切片实际上是个结构体，那么在`rust`里，它也是个结构体吗？
+
+- string.add
+    ~~~c
+    let s1 = String::from("hello");
+    let s2 = String::from("world");
+
+    // let s3 = s1.add(&s2);
+    let s3 = s1 + &s2;
+    ~~~
+
+    应该这样理解，比如let s = String::from("I like dogs")，这里并不代表String不能修改，而是s是String的一个不可变的绑定，你可以尝试下后面可以用let mut s1 = s，这个时候s1是String的一个可变的绑定，s1获取了s的所有权，函数调用的使用也是这样，因为获取的是所有权，在获取所有权时你可以决定是可变的绑定(s1)还是不可变的绑定(s)，因为你就是数据的拥有者了，可以自己决定改还是不改。这一点与接受引用的函数是不同的，接受引用其实就是借用，你不能说你是不可变的借用，然后又想要修改数据，所有者不会同意的。
