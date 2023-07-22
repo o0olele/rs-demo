b3b9aef5f31cf2974eeddbdb5362548c8d129486
o0olele <o0olele@github.com>
2023-07-22 22:04:04 +0800

    [15.flow_control] code and md

--- flow_control/src/main.rs
+++ flow_control/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     if_expressions();
+    for_repetition();
+    while_repetition();
+    loop_repetition();
 }
 
 fn if_expressions() {
@@ -19,6 +22,127 @@ fn if_expressions() {
     } else {
         println!("number is not divisible by 4, 3, or 2");
     }
+
+    println!();
+}
+
+#[derive(Debug)]
+struct User {
+    name: String,
+}
+
+fn for_repetition() {
+    for i in 1..=5 {
+        // [1,5]
+        println!("{}", i);
+    }
+
+    let u32_list: [u32; 5] = [1, 2, 3, 4, 5];
+    for val in u32_list {
+        println!("{}", val);
+    }
+    for val in &u32_list {
+        println!("{}", val);
+    }
+
+    let user_list: [User; 2] = [
+        User {
+            name: String::from("kinoko7"),
+        },
+        User {
+            name: String::from("yommyko"),
+        },
+    ];
+    // for val in user_list {
+    //     println!("{}", val.name)
+    // }
+    // println!("{:?}", user_list) // borrow of moved value: `user_list`
+    for val in &user_list {
+        println!("{}", val.name);
+        // val.name = String::from("ai"); // cannot assign to `val.name`, which is behind a `&` reference `val` is a `&` reference, so the data it refers to cannot be written
+    }
+    println!("{:?}", user_list);
+
+    let mut user_list: [User; 2] = [
+        User {
+            name: String::from("kinoko7"),
+        },
+        User {
+            name: String::from("yommyko"),
+        },
+    ];
+    for val in &mut user_list {
+        val.name = String::from("ai");
+    }
+
+    let arr = [11, 22, 33, 44];
+    for (i, v) in arr.iter().enumerate() {
+        println!("{} = {}", i, v);
+    }
+
+    for _ in 0..10 {
+        println!("repeat 10 times.")
+    }
+    for i in 0..10 {
+        _ = i;
+    }
+
+    println!();
+}
+
+fn while_repetition() {
+    let mut n = 0;
+    while n <= 5 {
+        println!("{}!", n);
+        n = n + 1;
+    }
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index = index + 1;
+    }
+
+    println!();
+}
+
+fn loop_repetition() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            if count == 3 {
+                continue 'counting_up; 
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 
     println!();
 }
--- flow_control/zh.md
+++ flow_control/zh.md
@@ -4,5 +4,68 @@
 
 # 运行结果
 ~~~shell
+The value of number is: 5
+number is divisible by 3
+
+1
+2
+3
+4
+5
+1
+2
+3
+4
+5
+1
+2
+3
+4
+5
+kinoko7
+yommyko
+[User { name: "kinoko7" }, User { name: "yommyko" }]
+0 = 11
+1 = 22
+2 = 33
+3 = 44
+repeat 10 times.
+repeat 10 times.
+repeat 10 times.
+repeat 10 times.
+repeat 10 times.
+repeat 10 times.
+repeat 10 times.
+repeat 10 times.
+repeat 10 times.
+repeat 10 times.
+
+0!
+1!
+2!
+3!
+4!
+5!
+the value is: 10
+the value is: 20
+the value is: 30
+the value is: 40
+the value is: 50
+
+The result is 20
+count = 0
+remaining = 10
+remaining = 9
+count = 1
+remaining = 10
+remaining = 9
+count = 2
+remaining = 10
+End count = 2
 
 ~~~
+
+# 注意事项
+- 第一种方式是循环索引，然后通过索引下标去访问集合，第二种方式是直接循环集合中的元素，优劣如下：
+    - 性能：第一种使用方式中 collection[index] 的索引访问，会因为边界检查(Bounds Checking)导致运行时的性能损耗 —— Rust 会检查并确认 index 是否落在集合内，但是第二种直接迭代的方式就不会触发这种检查，因为编译器会在编译时就完成分析并证明这种访问是合法的
+    - 安全：第一种方式里对 collection 的索引访问是非连续的，存在一定可能性在两次访问之间，collection 发生了变化，导致脏数据产生。而第二种直接迭代的方式是连续访问，因此不存在这种风险( 由于所有权限制，在访问过程中，数据并不会发生变化)。
