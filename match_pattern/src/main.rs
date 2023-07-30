1a525bfe5fcbf8f2ee2ccfdf63b743712abb8351
o0olele <o0olele@github.com>
2023-07-30 15:54:47 +0800

    [19.match_pattern

---
+++ match_pattern/src/main.rs
@@ -0,0 +1,121 @@
+fn main() {
+    literals();
+    named_variables();
+    multiple_patterns();
+    with_range();
+
+    destructuring_structs();
+    destructuring_enums();
+}
+
+fn literals() {
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    println!();
+}
+
+fn named_variables() {
+    let x = Some(5);
+    let y = 10;
+    // let x = Some(50);
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+    println!();
+}
+
+fn multiple_patterns() {
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+    println!();
+}
+
+fn with_range() {
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    println!();
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn destructuring_structs() {
+    let p = Point { x: 0, y: 7 };
+
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    let Point { x, y } = p;
+    // let Point { a, b } = p; //pattern does not mention fields `x`, `y`
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn destructuring_enums() {
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        // Message::Quit{} => {
+        //     println!("quit ");
+        // }
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+    
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {r}, green {g}, and blue {b}",)
+        }
+    }
+}
---
+++ match_pattern/zh.md
@@ -0,0 +1,5 @@
+# 原文
+- [全模式列表](https://course.rs/basic/match-pattern/all-patterns.html)
+- [Pattern Syntax](https://doc.rust-lang.org/book/ch18-03-pattern-syntax.html)
+
+# 运行结果
