b8dd15343b9b569b3d73ff331e1413778168c237
o0olele <o0olele@github.com>
2023-07-30 15:11:17 +0800

    [16.match_option]

---
+++ match_option/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    if let Some(i) = six {
+        println!("{}", i);
+    }
+
+    if let None = none {
+        println!("none is None!")
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
---
+++ match_option/zh.md
@@ -0,0 +1,9 @@
+# 原文
+- [解构 Option](https://course.rs/basic/match-pattern/option.html)
+- [Matching with Option<T>](https://doc.rust-lang.org/book/ch06-02-match.html#matching-with-optiont)
+
+# 运行结果
+~~~shell
+6
+none is None!
+~~~
