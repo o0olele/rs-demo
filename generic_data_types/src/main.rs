6b94075641c86aed1ee55b4a1609d0c8934c3ea0
o0olele <o0olele@github.com>
2023-08-06 14:08:58 +0800

    [21.generic_data_types] code

--- generic_data_types/src/main.rs
+++ generic_data_types/src/main.rs
@@ -3,6 +3,10 @@ fn main() {
     in_function_definitions();
     in_struct_definitions();
     in_enum_definitions();
+    in_method_definitions();
+    in_const();
+
+    practice();
 }
 
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
@@ -36,7 +40,7 @@ struct Point<T> {
 }
 
 #[derive(Debug)]
-struct DiffPoint<T,U> {
+struct DiffPoint<T, U> {
     x: T,
     y: U,
 }
@@ -44,7 +48,7 @@ struct DiffPoint<T,U> {
 fn in_struct_definitions() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
-    let p = DiffPoint{x: 1, y :1.1};
+    let p = DiffPoint { x: 1, y: 1.1 };
 
     println!("{:?} {:?} {:?}", integer, float, p);
     println!()
@@ -55,4 +59,90 @@ fn in_enum_definitions() -> Result<i32, f32> {
         return Err(2f32);
     }
     return Ok(1);
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+impl<T, U> DiffPoint<T, U> {
+    fn mixup<V, W>(self, other: DiffPoint<V, W>) -> DiffPoint<T, W> {
+        DiffPoint {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn in_method_definitions() {
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
+
+    let p1 = DiffPoint { x: 5, y: 10.4 };
+    let p2 = DiffPoint { x: "Hello", y: 'c'};
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    println!();
+}
+
+fn display_array<T: std::fmt::Debug, const N: usize>(arr: [T; N]) {
+    println!("{:?}", arr);
+}
+
+fn in_const() {
+    let arr: [i32; 3] = [1, 2, 3];
+    display_array(arr);
+
+    let arr: [i32; 2] = [1, 2];
+    display_array(arr);
+    println!();
+
+    let a= S;
+}
+
+
+// enum Option_i32 {
+//     Some(i32),
+//     None,
+// }
+
+// enum Option_f64 {
+//     Some(f64),
+//     None,
+// }
+
+struct A;          // Concrete type `A`.
+struct S(A);       // Concrete type `S`.
+struct SGen<T>(T); // Generic type `SGen`.
+
+fn reg_fn(_s: S) {}
+
+fn gen_spec_t(_s: SGen<A>) {}
+
+fn gen_spec_i32(_s: SGen<i32>) {}
+
+fn generic<T>(_s: SGen<T>) {}
+
+fn practice() {
+    // Using the non-generic functions
+    reg_fn(S(A));          // Concrete type.
+    gen_spec_t(SGen(A));   // Implicitly specified type parameter `A`.
+    gen_spec_i32(SGen(6)); // Implicitly specified type parameter `i32`.
+
+    // Explicitly specified type parameter `char` to `generic()`.
+    generic::<char>(SGen('a'));
+
+    // Implicitly specified type parameter `char` to `generic()`.
+    generic(SGen('c'));
 }
--- generic_data_types/zh.md
+++ generic_data_types/zh.md
@@ -3,3 +3,15 @@
 - [Generic Data Types](https://doc.rust-lang.org/book/ch10-01-syntax.html)
 
 # 运行结果
+~~~shell
+The largest number is 100
+The largest char is y
+Point { x: 5, y: 10 } Point { x: 1.0, y: 4.0 } DiffPoint { x: 1, y: 1.1 }
+
+p.x = 5
+p3.x = 5, p3.y = c
+
+[1, 2, 3]
+[1, 2]
+
+~~~
